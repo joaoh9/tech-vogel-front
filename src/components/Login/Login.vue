55deb12a38ce312fa983402be517ffc2df7a23dc
Breno <brenoam@dcc.ufmg.br>
2020-05-13 18:36:11 -0300

    Sign Up

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -5,40 +5,122 @@
         <v-btn class="ml-2" color="info" text v-on="on">Login</v-btn>
       </template>
       <v-card>
-        <v-card-title>
-          <span class="headline">Sign In</span>
-        </v-card-title>
-        <v-card-text>
-          <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-          >
-            <v-container>
-              <v-row>
-                <v-col cols='12'>
-                  <v-text-field prepend-icon="fa-user" label="Email" required></v-text-field>
-                </v-col>
-                <v-col cols='12'>
-                  <v-text-field prepend-icon="fa-lock" label="Password" type="password" required></v-text-field>
-                </v-col>
-                <v-col
-                  cols='12'
-                  align="center"
-                >
-                  <v-btn
-                    rounded
-                    color="success"
-                    class="mr-4"
-                    @click="validate; dialog = false"
+        <v-tabs v-model="tab" grow>
+          <v-tab key=0>Sign In</v-tab>
+          <v-tab key=1>Sign Up</v-tab>
+          <v-tabs-items v-model="tab">
+            <v-tab-item key=0>
+              <v-card flat>
+                <v-card-text>
+                  <v-form
+                    ref="form"
+                    v-model="valid"
+                    lazy-validation
                   >
-                    Sign In
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-        </v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols='12'>
+                          <v-text-field
+                            name="username"
+                            prepend-icon="fa-user"
+                            label="Username or Email"
+                            required
+                        ></v-text-field>
+                        </v-col>
+                        <v-col cols='12'>
+                          <v-text-field
+                            name="password"
+                            prepend-icon="fa-lock"
+                            label="Password"
+                            type="password"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                        <v-col
+                          cols='12'
+                          align="center"
+                        >
+                          <v-btn
+                            rounded
+                            large
+                            color="primary"
+                            class="mr-4"
+                            @click="validate; dialog = false"
+                          >
+                            Sign In
+                          </v-btn>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-tab-item>
+            <v-tab-item key=1>
+              <v-card flat>
+                <v-card-text>
+                  <v-form
+                    ref="form"
+                    v-model="valid"
+                    lazy-validation
+                  >
+                    <v-container>
+                      <v-row>
+                        <v-col cols='12'>
+                          <v-text-field
+                            name="username"
+                            prepend-icon="fa-user"
+                            label="Username"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols='12'>
+                          <v-text-field
+                            name="email"
+                            prepend-icon="fa-envelope"
+                            label="Email"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols='12'>
+                          <v-text-field
+                            name="password"
+                            prepend-icon="fa-lock"
+                            label="Password"
+                            type="password"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols='12'>
+                          <small>
+                            By signing up, you agree with our
+                            <a target="_blank" href="">Terms and Conditions</a>
+                            and our
+                            <a target="_blank" href="">Privacy Policy</a>
+                          </small>
+                        </v-col>
+                        <v-col
+                          cols='12'
+                          align="center"
+                        >
+                          <v-btn
+                            rounded
+                            large
+                            color="primary"
+                            class="mr-4"
+                            @click="validate; dialog = false"
+                          >
+                            Sign Up
+                          </v-btn>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-tabs>
       </v-card>
     </v-dialog>
   </div>
@@ -50,6 +132,7 @@ export default {
   data() {
     return {
       dialog: false,
+      tab: 0,
     };
   },
 };
