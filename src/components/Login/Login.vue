32fd6467f266c97c6810474bb25bad266c618c41
João Henrique <joaoh9.costa@gmail.com>
2020-05-23 15:10:47 -0300

    tile on login

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -2,7 +2,7 @@
   <div>
     <v-dialog v-model="dialog" max-width="600px">
       <template v-slot:activator="{ on }">
-        <v-btn class="ml-2" color="info" text v-on="on">Login</v-btn>
+        <v-btn tile class="ml-2" color="info" text v-on="on">Login</v-btn>
       </template>
       <v-card>
         <v-tabs v-model="tab" grow>
@@ -15,7 +15,8 @@
                   <v-form ref="form" v-model="valid" lazy-validation>
                     <v-container>
                       <v-text-field
-                        rounded
+                        tile
+                        style="opacity: 80%"
                         outlined
                         name="username"
                         prepend-inner-icon="fa-user"
@@ -23,7 +24,8 @@
                         required
                       ></v-text-field>
                       <v-text-field
-                        rounded
+                        tile
+                        style="opacity: 80%"
                         outlined
                         name="password"
                         prepend-inner-icon="fa-lock"
@@ -32,11 +34,12 @@
                         required
                       ></v-text-field>
                       <v-btn
-                        rounded
+                        tile
+                        style="opacity: 80%"
                         large
                         color="primary"
                         class="mt-2"
-                        @click="validate; dialog = false"
+                        @click="saveUser; dialog = false"
                       >Log In</v-btn>
                     </v-container>
                   </v-form>
@@ -49,23 +52,62 @@
                   <v-form ref="form" v-model="valid" lazy-validation>
                     <v-container>
                       <v-text-field
-                        rounded
+                        tile
+                        style="opacity: 80%"
+                        outlined
+                        name="Name"
+                        prepend-inner-icon="fa-user"
+                        label="Name"
+                        required
+                        v-model="name"
+                      ></v-text-field>
+                      {{name}}
+                      <v-text-field
+                        tile
+                        style="opacity: 80%"
                         outlined
                         name="username"
                         prepend-inner-icon="fa-user"
                         label="Username"
                         required
+                        v-model="username"
                       ></v-text-field>
-                      <v-text-field rounded outlined name="email" prepend-inner-icon="fa-envelope" label="Email" required></v-text-field>
+                      {{username}}
                       <v-text-field
-                        rounded
+                        tile
+                        style="opacity: 80%"
+                        outlined
+                        name="email"
+                        prepend-inner-icon="fa-envelope"
+                        label="Email"
+                        required
+                        v-model="email"
+                      ></v-text-field>
+                      {{email}}
+                      <v-text-field
+                        tile
+                        style="opacity: 80%"
                         outlined
                         name="password"
                         prepend-inner-icon="fa-lock"
                         label="Password"
                         type="password"
                         required
+                        v-model="password"
                       ></v-text-field>
+                      {{password}}
+                      <v-text-field
+                        tile
+                        style="opacity: 80%"
+                        outlined
+                        name="Confirm Password"
+                        prepend-inner-icon="fa-lock"
+                        label="Confirm Password"
+                        type="password"
+                        required
+                        v-model="confirmPassword"
+                      ></v-text-field>
+                      {{confirmPassword}}
                       <v-checkbox v-model="checkbox">
                         <small slot="label">
                           By signing up, you agree with our
@@ -75,11 +117,12 @@
                         </small>
                       </v-checkbox>
                       <v-btn
-                        rounded
+                        tile
+                        style="opacity: 80%"
                         large
                         color="primary"
                         class="mt-2"
-                        @click="validate; dialog = false"
+                        @click="saveUser()"
                       >Sign Up</v-btn>
                     </v-container>
                   </v-form>
@@ -94,6 +137,8 @@
 </template>
 
 <script>
+import UserController from '../../controllers/user-controller';
+
 export default {
   name: 'Login',
   data() {
@@ -101,7 +146,36 @@ export default {
       dialog: false,
       tab: 0,
       checkbox: false,
+      name: '',
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
     };
+  },
+  methods: {
+    saveUser() {
+      const userController = new UserController();
+      if (this.validateForm()) {
+        userController
+          .saveUser({
+            name: this.name,
+            username: this.username,
+            email: this.email,
+            password: this.password,
+          })
+          .then((res) => {
+            console.log(res);
+            this.$router.push({ path: '/' });
+          })
+          .catch((e) => {
+            console.log('Erro ao cadastrar usuário!', e);
+          });
+      }
+    },
+    validateForm() {
+      return this.name && this.username && this.email && this.password === this.confirmPassword;
+    },
   },
 };
 </script>
