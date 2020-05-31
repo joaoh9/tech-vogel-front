86ecd1fbc1591fa5fe528054020ddd4f5f46c2bb
João Henrique <joaoh9.costa@gmail.com>
2020-05-31 12:42:37 -0300

    jobcard layout change

--- src/components/Card/JobCard.vue
+++ src/components/Card/JobCard.vue
@@ -1,30 +1,7 @@
 <template>
   <v-card class="mx-auto" max-width="800" elevation="6">
-    <v-list-item three-line>
-      <v-list-item-content>
-        <v-list-item-title class="headline mb-1">{{ jobTitle }}</v-list-item-title>
-        <v-list-item-subtitle>
-          <v-chip pill color="blue lighten-2" class="ma-2">
-            <v-avatar color="blue darken-2" left>
-              <i class="devicon-angularjs-plain" style="font-size: 18px"></i>
-            </v-avatar>Angular.js
-          </v-chip>
-          <v-chip class="ma-2" pill color="green lighten-2">
-            <v-avatar color="green darken-2" left>
-              <i class="devicon-nodejs-plain" style="font-size: 18px"></i>
-            </v-avatar>Node.js
-          </v-chip>
-          <v-chip class="ma-2" pill color="teal lighten-4">
-            <v-avatar color="teal lighten-2" left>
-              <i class="devicon-vuejs-plain" style="font-size: 18px"></i>
-            </v-avatar>Vue.js
-          </v-chip>
-        </v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-group>
-        <h3 style="text-align: center">Apple</h3>
-        <div>Oslo, Norway</div>
-      </v-list-item-group>
+    <v-list-item>
+      <div class="d-flex justify-space-between">
       <v-list-item-avatar tile size="80" color="white">
         <v-img
           contain
@@ -32,6 +9,30 @@
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/647px-Apple_logo_black.svg.png"
         ></v-img>
       </v-list-item-avatar>
+      <v-list-item-group class="ma-3 mr-5">
+        <h3 style="text-align: center">Apple</h3>
+        <div>Oslo, Norway</div>
+      </v-list-item-group>
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1">{{ jobTest.title }}</v-list-item-title>
+        <v-list-item-subtitle>
+          <div>
+            <v-chip
+              class="mr-2 mb-1 mt-1"
+              color="cyan lighten-5"
+            >{{this.jobTest.skills.knowledgeAreas[0]}}</v-chip>
+            <v-chip
+              class="mr-2 mb-1 mt-1"
+              color="blue lighten-5"
+            >{{this.jobTest.skills.programmingLanguages[0]}}</v-chip>
+            <v-chip
+              class="mr-2 mb-1 mt-1"
+              color="purple lighten-5"
+            >{{this.jobTest.skills.frameworks[0]}}</v-chip>
+          </div>
+        </v-list-item-subtitle>
+      </v-list-item-content>
+      </div>
     </v-list-item>
   </v-card>
 </template>
@@ -41,7 +42,15 @@ export default {
   name: 'JobCard',
   data() {
     return {
-      jobTitle: 'Full Stack Developer',
+      jobTest: {
+        title: 'Full Stack Developer',
+        skills: {
+          knowledgeAreas: ['Front-end programming', 'Back-end programming'],
+          programmingLanguages: ['JavaScript'],
+          frameworks: ['Node.js', 'React.js', 'Amazon-web-services', 'graphql'],
+          softSkills: ['Communicative'],
+        },
+      },
     };
   },
 };
