0822a67054cac51622df79af416d16fef05aca32
João Henrique <joaoh9.costa@gmail.com>
2020-05-31 12:43:23 -0300

    navbar logo and turns it into white

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,22 +1,33 @@
 <template>
-  <div>
-    <v-app-bar color="grey darken-4" dark>
-      <v-btn to="/" tile color="grey-lighten-2" class="mr-5">Tech Vogel</v-btn>
-      <v-btn to="howitworks" tile color="grey lighten-3" text>How It Works</v-btn>
-      <v-btn to="aboutus" tile color="grey lighten-3" text>About Us</v-btn>
-      <v-btn to="pricing" tile color="grey lighten-3" text>Pricing</v-btn>
-      <v-btn to="content" tile color="grey lighten-3" text>Content</v-btn>
-      <v-spacer></v-spacer>
-      <v-btn class="" color="info" tile elevation="12">Apply</v-btn>
-      <v-btn class="ml-3" color="info" tile elevation="12" outlined>Hire</v-btn>
-      <Login> </Login>
-      <!-- <v-btn class="ml-2" color="info" text>Log In</v-btn> -->
-    </v-app-bar>
-  </div>
+  <v-app-bar
+    scroll-target="scroll-body"
+    :hide-on-scroll="false"
+    elevate-on-scroll
+    color="white darken-4"
+  >
+    <v-btn text color="white darken-4" height="60" tile elevation="0" to="/">
+      <v-avatar size="150">
+        <v-img height="100" width="1" :src="logo"></v-img>
+      </v-avatar>
+    </v-btn>
+    <!-- <v-btn to="/" tile color="grey-lighten-2" class="mr-5">Tech Vogel</v-btn> -->
+    <v-btn to="howitworks" color="grey darken-3" text>How It Works</v-btn>
+    <v-btn to="aboutus" color="grey darken-3" text>About Us</v-btn>
+    <v-btn to="pricing" color="grey darken-3" text>Pricing</v-btn>
+    <v-btn to="content" color="grey darken-3" text>Content</v-btn>
+    <v-spacer></v-spacer>
+    <v-btn class color="info" elevation="12">Apply</v-btn>
+    <v-btn class="ml-3" color="info" elevation="12" outlined>Hire</v-btn>
+    <Login></Login>
+    <v-sheet id="scroll-body" class="overflow-y-auto" max-height="600">
+      <v-container style="height: 1000px;"></v-container>
+    </v-sheet>
+  </v-app-bar>
 </template>
 
 <script>
 import Login from '../Login/Login.vue';
+import Logo from '../../assets/tv-new-logo-squared-blue.png';
 
 export default {
   name: 'Navbar',
@@ -24,6 +35,9 @@ export default {
     Login,
   },
   data() {
+    return {
+      logo: Logo,
+    };
   },
 };
 </script>
