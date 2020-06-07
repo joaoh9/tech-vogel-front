4c9d6ce57caef04a3a7a3e0699630edf217f3b71
João Henrique <joaoh9.costa@gmail.com>
2020-06-07 03:15:37 -0300

    navbar elevate

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -3,27 +3,23 @@
     <!-- scroll-target="scroll-body"
     :hide-on-scroll="false"
     shrink-on-scroll
-    elevate-on-scroll -->
-  <v-app-bar
-    app
-    color="white darken-4"
-    elevate-on-scroll
-  >
-    <v-btn text color="white darken-4" height="60" tile elevation="0" to="/">
-      <v-avatar size="150">
-        <v-img height="100" width="1" :src="logo"></v-img>
-      </v-avatar>
-    </v-btn>
-    <!-- <v-btn to="/" tile color="grey-lighten-2" class="mr-5">Tech Vogel</v-btn> -->
-    <v-btn to="howitworks" color="grey darken-3" text>How It Works</v-btn>
-    <v-btn to="aboutus" color="grey darken-3" text>About Us</v-btn>
-    <v-btn to="pricing" color="grey darken-3" text>Pricing</v-btn>
-    <v-btn to="content" color="grey darken-3" text>Content</v-btn>
-    <v-spacer></v-spacer>
-    <v-btn class color="info" elevation="12">Apply</v-btn>
-    <v-btn class="ml-3" color="info" elevation="12" outlined>Hire</v-btn>
-    <Login></Login>
-  </v-app-bar>
+    elevate-on-scroll-->
+    <v-app-bar app color="white darken-4" elevate-on-scroll>
+      <v-btn text color="white darken-4" height="60" tile elevation="0" to="/">
+        <v-avatar size="150">
+          <v-img height="100" width="1" :src="logo"></v-img>
+        </v-avatar>
+      </v-btn>
+      <!-- <v-btn to="/" tile color="grey-lighten-2" class="mr-5">Tech Vogel</v-btn> -->
+      <v-btn to="howitworks" color="grey darken-3" text>How It Works</v-btn>
+      <v-btn to="aboutus" color="grey darken-3" text>About Us</v-btn>
+      <v-btn to="pricing" color="grey darken-3" text>Pricing</v-btn>
+      <v-btn to="content" color="grey darken-3" text>Content</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn class color="info" elevation="12">Apply</v-btn>
+      <v-btn class="ml-3" color="info" elevation="12" outlined>Hire</v-btn>
+      <Login></Login>
+    </v-app-bar>
   </div>
 </template>
 
@@ -43,3 +39,10 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.v-toolbar {
+  box-shadow: 0px 2px 8px -1px rgba(0, 0, 0, 0.2), 0px 8px 5px 0px rgba(0, 0, 0, 0.14),
+    0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+}
+</style>
