a945787aba4ff57af78301fa73a421d07a4915c3
João Henrique <joaoh9.costa@gmail.com>
2020-06-17 20:37:44 -0300

    lint

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  transpileDependencies: [
-    'vuetify',
-  ],
+  transpileDependencies: ['vuetify'],
   devServer: {
     proxy: {
       '^/api': {
@@ -14,18 +12,19 @@ module.exports = {
       },
     },
   },
-  configureWebpack: { resolve: {
-    extensions: [ '.js', '.vue' ],
-    alias: {
-      Interface: path.resolve(__dirname, 'src/components/Interface/'),
-      Components: path.resolve(__dirname, 'src/components/'),
-      Controllers: path.resolve(__dirname, 'src/controllers/'),
-      Helpers: path.resolve(__dirname, 'src/helpers/'),
-      Assets: path.resolve(__dirname, 'src/assets/'),
-      Data: path.resolve(__dirname, 'src/data/'),
-      'Oficiona': path.resolve(__dirname, 'oficiona'),
-      '@': path.resolve(__dirname, 'src'),
+  configureWebpack: {
+    resolve: {
+      extensions: [ '.js', '.vue' ],
+      alias: {
+        Interface: path.resolve(__dirname, 'src/components/Interface/'),
+        Components: path.resolve(__dirname, 'src/components/'),
+        Controllers: path.resolve(__dirname, 'src/controllers/'),
+        Helpers: path.resolve(__dirname, 'src/helpers/'),
+        Assets: path.resolve(__dirname, 'src/assets/'),
+        Data: path.resolve(__dirname, 'src/data/'),
+        Oficiona: path.resolve(__dirname, 'oficiona'),
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
   },
 };
