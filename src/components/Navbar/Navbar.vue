3554bc07f8b27ab07b1195c18b19dffabf5fae0d
João Henrique <joaoh9.costa@gmail.com>
2020-07-23 01:08:06 -0300

    fix button link narvbar

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -7,13 +7,13 @@
         </v-avatar>
       </v-btn>
       <v-spacer></v-spacer>
-      <v-btn to="jobList" color="grey darken-3" text>Jobs</v-btn>
-      <v-btn to="pricing" color="grey darken-3" text>Pricing</v-btn>
-      <v-btn to="howItWorks" color="grey darken-3" text>How It Works</v-btn>
+      <v-btn @click="goToJobList" color="grey darken-3" text>Jobs</v-btn>
+      <v-btn @click="goToPricing" color="grey darken-3" text>Pricing</v-btn>
+      <v-btn @click="goToHowItWorks" color="grey darken-3" text>How It Works</v-btn>
       <v-spacer></v-spacer>
       <!-- <v-btn to="content" color="grey darken-3" text>Sign Up</v-btn> -->
-      <v-btn to="signup" text>Sign Up</v-btn>
-      <v-btn to="login" tile large color="primary" class="mx-4">Login</v-btn>
+      <v-btn @click="goToSignup" text>Sign Up</v-btn>
+      <v-btn @click="goToLogin" tile large color="primary" class="mx-4">Login</v-btn>
     </v-app-bar>
   </div>
 </template>
@@ -23,19 +23,32 @@ import Logo from 'Assets/logo-escrita-preto-amarelo.svg';
 
 export default {
   name: 'Navbar',
-  components: {
-  },
+  components: {},
   data() {
     return {
       logo: Logo,
     };
+  },
+  methods: {
+    goToSignup() {
+      this.$router.push({
+        name: 'Signup',
+      });
+    },
+    goToLogin() {
+      this.$router.push({
+        name: 'Login',
+      });
+    },
+    goToJobList() {},
+    goToPricing() {},
+    goToHowItWorks() {},
   },
 };
 </script>
 
 <style scoped>
 .v-toolbar {
-  box-shadow: 0px 2px 8px -1px rgba(0, 0, 0, 0.2), 0px 8px 5px 0px rgba(0, 0, 0, 0.14),
-    0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  box-shadow: 0px 2px 8px -1px rgba(0, 0, 0, 0.2), 0px 8px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 }
 </style>
