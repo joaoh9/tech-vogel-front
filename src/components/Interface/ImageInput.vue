7a21d810c2e34432f32e1a3f8d2f789e78ee6fcd
Breno <brenoam@dcc.ufmg.br>
2020-08-26 00:46:33 -0300

    Image input component

---
+++ src/components/Interface/ImageInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-avatar color="light-gray" size="128">
+      <v-img :src="base64" alt="Company" />
+    </v-avatar>
+    <v-file-input
+      v-model="image"
+      hide-input
+      @change="atFileChange"
+    ></v-file-input>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ImageInput',
+  props: {},
+  components: {
+  },
+  data() {
+    return {
+      image: null,
+      imageUrl: null,
+      base64: '',
+    };
+  },
+  methods: {
+    atFileChange(){
+      console.log(this.image)
+      if (this.image) {
+        const fr = new FileReader()
+        fr.onload = function(){
+          fr.base64 = fr.result;
+        };
+        fr.readAsDataURL(this.image)
+        this.$emit('input', this.image)
+      }
+    },
+  },
+};
+</script>
+
