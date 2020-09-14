a9a0f09163c3355dd36deead2948fece7fd0699b
João Henrique <joaoh9.costa@gmail.com>
2020-09-13 23:56:23 -0300

    acerta componentes

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -17,8 +17,8 @@
       <v-btn class="body-2" @click="goToHowItWorks" color="grey darken-3" text>How It Works</v-btn>
       <v-spacer></v-spacer>
       <!-- <v-btn to="content" color="grey darken-3" text>Sign Up</v-btn> -->
-      <v-btn @click="goToSignup" text>Sign Up</v-btn>
-      <v-btn @click="goToLogin" tile large color="primary" class="mx-4">Login</v-btn>
+      <v-btn @click="goToSignup" color="secondary" tile outlined text large>Sign Up</v-btn>
+      <v-btn @click="goToLogin" tile elevation="0" large color="primary" class="mx-4">Login</v-btn>
     </v-app-bar>
   </div>
 </template>
