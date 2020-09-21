c73f0cd1bfab15fbe7db4f66c68abcefdfdf1a8e
João Henrique <joaoh9.costa@gmail.com>
2020-09-21 01:12:28 -0300

    parametro height no g-card, tira css de g-card-header

--- src/components/global/GCard.vue
+++ src/components/global/GCard.vue
@@ -3,8 +3,10 @@
     class="primary-card"
     :elevation="$vuetify.breakpoint.smAndUp ? 6 : 0"
     color="bg"
-    max-width="600"
     :min-width="minWidth()"
+    :max-width="maxWidth()"
+    :min-height="minHeight()"
+    :max-height="maxHeight()"
   >
     <slot name="card-content"> </slot>
   </v-card>
@@ -14,18 +16,25 @@
 export default {
   name: 'g-card',
   props: {
-    lg: {
-      type: Number,
-    },
-    md: {
-      type: Number,
-    },
-    sm: {
-      type: Number,
-    },
-    base: {
-      type: Number,
-    },
+    lg: Number,
+    md: Number,
+    sm: Number,
+    base: Number,
+
+    maxlg: Number,
+    maxmd: Number,
+    maxsm: Number,
+    maxbase: Number,
+
+    minheigthlg: Number,
+    minheigthmd: Number,
+    minheigthsm: Number,
+    minheigthbase: Number,
+
+    maxheightlg: Number,
+    maxheightmd: Number,
+    maxheightsm: Number,
+    maxheightbase: Number,
   },
   methods: {
     minWidth() {
@@ -39,6 +48,42 @@ export default {
         return this.sm || 320;
       }
       return this.base || 250;
+    },
+    maxWidth() {
+      if (this.$vuetify.breakpoint.lgAndUp) {
+        return this.maxlg || 1000;
+      }
+      if (this.$vuetify.breakpoint.mdAndUp) {
+        return this.maxmd || 800;
+      }
+      if (this.$vuetify.breakpoint.smAndUp) {
+        return this.maxsm || 320;
+      }
+      return this.maxbase || 250;
+    },
+    minHeight() {
+      if (this.$vuetify.breakpoint.lgAndUp) {
+        return this.minheigthlg;
+      }
+      if (this.$vuetify.breakpoint.mdAndUp) {
+        return this.minheigthmd;
+      }
+      if (this.$vuetify.breakpoint.smAndUp) {
+        return this.minheigthsm;
+      }
+      return this.minheigthbase;
+    },
+    maxHeight() {
+      if (this.$vuetify.breakpoint.lgAndUp) {
+        return this.maxheightlg;
+      }
+      if (this.$vuetify.breakpoint.mdAndUp) {
+        return this.maxheightmd;
+      }
+      if (this.$vuetify.breakpoint.smAndUp) {
+        return this.maxheightsm;
+      }
+      return this.maxheightbase;
     },
   },
 };
