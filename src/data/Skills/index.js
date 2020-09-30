e03272e23fc1ac189a5501d29d8665f5b38d9a49
João Henrique <joaoh9.costa@gmail.com>
2020-09-29 22:20:19 -0300

    programming languages and database json data

--- src/data/Skills/index.js
+++ src/data/Skills/index.js
@@ -1,10 +1,10 @@
-import FrameWorks2 from './Frameworks2.json'
+import FrameWorks from './Frameworks.json'
 import KnowledgeAreas from './KnowledgeAreas.json'
 import ProgrammingLanguages from './ProgrammingLanguages.json'
 import SoftSkills from './SoftSkills.json'
 
 export default {
-  FrameWorks2,
+  FrameWorks,
   KnowledgeAreas,
   ProgrammingLanguages,
   SoftSkills,
