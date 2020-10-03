c6bdb1a043a2b12c2a5ba3e27f26521f044240f7
João Henrique <joaoh9.costa@gmail.com>
2020-10-03 16:35:39 -0300

    skills format

--- src/data/Skills/index.js
+++ src/data/Skills/index.js
@@ -1,11 +1,15 @@
-import FrameWorks from './Frameworks.json'
-import KnowledgeAreas from './KnowledgeAreas.json'
-import ProgrammingLanguages from './ProgrammingLanguages.json'
-import SoftSkills from './SoftSkills.json'
+import Databases from './Databases.json';
+import FrameWorks from './Frameworks.json';
+import FrameWorksByGroup from './FrameworksByGroup.json';
+import ProgrammingLanguages from './ProgrammingLanguages.json';
+import SoftSkills from './SoftSkills.json';
+import SoftSkillsFlat from './SoftSkillsFlat.json';
 
 export default {
+  Databases,
   FrameWorks,
-  KnowledgeAreas,
+  FrameWorksByGroup,
   ProgrammingLanguages,
   SoftSkills,
-}
+  SoftSkillsFlat,
+};
