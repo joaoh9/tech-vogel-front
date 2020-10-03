63efc2cdf3c7ee52d675af24df02ced0531563e6
João Henrique <joaoh9.costa@gmail.com>
2020-10-02 23:15:09 -0300

    tira Mẽs de work experience

--- src/Pages/Resume/WorkItem.vue
+++ src/Pages/Resume/WorkItem.vue
@@ -7,7 +7,7 @@
       :placeholder="$t('CV.register.workExperience.placeholders.company.title')"
       outlined
     />
-    <form-input class="mt-6" :title="$t('CV.register.workExperience.position')" />
+    <form-input class="" :title="$t('CV.register.workExperience.position')" />
     <v-text-field
       v-model="job.position"
       v-on:input="$emit('update-item', job)"
@@ -15,53 +15,26 @@
       outlined
     />
     <v-row>
-      <v-col cols="12" md="6">
-        <form-input class="mt-4" :title="$t('Common.from')" />
-        <v-row>
-          <v-col>
-            <v-combobox
-              v-model="job.from.month"
-              v-on:input="$emit('update-item', job)"
-              :placeholder="$t('Common.month')"
-              :items="$t('Data.monthCount')"
-              outlined
-            />
-          </v-col>
-          <v-col>
-            <v-text-field
-              v-model="job.from.year"
-              v-on:input="$emit('update-item', job)"
-              :placeholder="$t('Common.year')"
-              outlined
-              :rules="[rules.year]"
-            />
-          </v-col>
-        </v-row>
+      <v-col cols="12" md="3">
+        <form-input class="" :title="$t('Common.from')" />
+        <v-text-field
+          v-model="job.from.year"
+          v-on:input="$emit('update-item', job)"
+          :placeholder="$t('Common.year')"
+          outlined
+          :rules="[rules.year]"
+        />
       </v-col>
-      <v-col cols="12" md="6">
-        <form-input class="mt-4" :title="$t('Common.to')" />
-        <v-row>
-          <v-col>
-            <v-combobox
-              v-model="job.to.month"
-              v-on:input="$emit('update-item', job)"
-              :placeholder="$t('Common.month')"
-              :items="months"
-              outlined
-              :disabled="job.currentJob"
-            />
-          </v-col>
-          <v-col>
-            <v-text-field
-              v-model="job.to.year"
-              v-on:input="$emit('update-item', job)"
-              :placeholder="$t('Common.year')"
-              outlined
-              :rules="[rules.year]"
-              :disabled="job.currentJob"
-            />
-          </v-col>
-        </v-row>
+      <v-col cols="12" md="3">
+        <form-input class="" :title="$t('Common.to')" />
+        <v-text-field
+          v-model="job.to.year"
+          v-on:input="$emit('update-item', job)"
+          :placeholder="$t('Common.year')"
+          outlined
+          :rules="[rules.year]"
+          :disabled="job.currentJob"
+        />
       </v-col>
     </v-row>
     <div class="d-flex justify-space-between mt-12">
@@ -95,11 +68,9 @@ export default {
         },
         position: '',
         from: {
-          month: '',
           year: '',
         },
         to: {
-          month: '',
           year: '',
         },
         jobDescription: '',
