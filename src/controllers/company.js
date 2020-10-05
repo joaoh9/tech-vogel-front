e0e0f75ba06404ae9cdffbfe7ac0194e43214499
João Henrique <joaoh9.costa@gmail.com>
2020-10-05 19:00:21 -0300

    company controller update

--- src/controllers/company.js
+++ src/controllers/company.js
@@ -2,12 +2,13 @@ import Axios from 'Helpers/axios';
 
 export default class CompanyController {
   async registerCompany(details) {
+    details.companyId = details.name.replace(/ /g, '-');
     const axios = await Axios.GetInstance();
     const res = await axios.post('/company', details);
     return res;
   }
 
-  async getCompanyById(companyId){
+  async getCompanyById(companyId) {
     const axios = await Axios.GetInstance();
     const res = await axios.get(`/company/${companyId}`);
     return res;
