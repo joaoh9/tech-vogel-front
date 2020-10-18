51d2b3d9c75ddcf7cd7f88a0abaf03e1ef66ee91
João Henrique <joaoh9.costa@gmail.com>
2020-10-18 01:16:32 -0300

    default not found page

---
+++ src/Pages/Static/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <PrimaryHeader title="404 - Page not found" description="Uh-oh" />
+    <div class="d-flex justify-center mt-6">
+      <g-btn to="/" type="primary" label="Click here to return home" />
+    </div>
+  </div>
+</template>
+
+<script>
+import PrimaryHeader from 'Components/Interface/PrimaryHeader';
+
+export default {
+  name: '404-NotFound',
+  components: {
+    PrimaryHeader,
+  },
+};
+</script>
+
+<style></style>
--- src/components/Interface/PrimaryHeader.vue
+++ src/components/Interface/PrimaryHeader.vue
@@ -2,6 +2,7 @@
   <v-card color="primary" class="pa-16" flat style="border-radius: 0px !important">
     <h3 class="h3-bold color-light">{{ title }}</h3>
     <h5 class="color-light mt-4">{{ description }}</h5>
+    <slot name="default" />
   </v-card>
 </template>
 
