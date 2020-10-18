61d4d0f7b834fd48204497c858365816bc863ffb
João Henrique <joaoh9.costa@gmail.com>
2020-10-18 19:25:14 -0300

    elementos de texto global

---
+++ src/components/global/Text/body-1.vue
@@ -0,0 +1,17 @@
+<template>
+  <p :class="`${color} bdy-1`"><slot name="default"></slot></p>
+</template>
+
+<script>
+export default {
+  name: 'bdy-1',
+  props: {
+    color: {
+      type: String,
+      default: 'color-cinza-lighten-1',
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/global/Text/body-2.vue
@@ -0,0 +1,17 @@
+<template>
+  <p :class="`${color} bdy-2`"><slot name="default"></slot></p>
+</template>
+
+<script>
+export default {
+  name: 'bdy-2',
+  props: {
+    color: {
+      type: String,
+      default: 'color-cinza-lighten-1',
+    },
+  },
+};
+</script>
+
+<style></style>
