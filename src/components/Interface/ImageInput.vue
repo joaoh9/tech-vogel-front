52a4bef2cbbfa59d50d94f7a1f0ed04830f6687b
João Henrique <joaoh9.costa@gmail.com>
2020-10-18 03:20:52 -0300

    upload de imagem

--- src/components/Interface/ImageInput.vue
+++ src/components/Interface/ImageInput.vue
@@ -3,21 +3,15 @@
     <v-avatar color="light-gray" size="128">
       <v-img :src="base64" alt="Company" />
     </v-avatar>
-    <v-file-input
-      v-model="image"
-      hide-input
-      @change="atFileChange"
-    />
+    <v-file-input v-model="image" hide-input @change="handleFileUpload" />
   </div>
 </template>
 
 <script>
-
+const MB = 1000000;
+const MAX_FILE_SIZE = 10 * MB;
 export default {
   name: 'ImageInput',
-  props: {},
-  components: {
-  },
   data() {
     return {
       image: null,
@@ -26,17 +20,37 @@ export default {
     };
   },
   methods: {
-    atFileChange(){
-      if (this.image) {
-        const fr = new FileReader()
-        fr.onload = function(){
-          fr.base64 = fr.result;
-        };
-        fr.readAsDataURL(this.image)
-        this.$emit('input', this.image)
-      }
+    handleFileUpload(e) {
+      e.target.files.forEach(file => {
+        if (file.size > MAX_FILE_SIZE) {
+          this.$toast.e(`Arquivo ${file.name} exceede limite de ${MAX_FILE_SIZE / MB}MB`);
+          return;
+        }
+        this.processDocument(file);
+      });
+    },
+    getBase64(file) {
+      return new Promise(resolve => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+      });
+    },
+    processDocument(file) {
+      this.getBase64(file)
+        .then(data => {
+          this.event.files.push({
+            data64: data,
+            fileSize: file.size.toString(),
+            mimeType: file.type,
+            fileName: file.name,
+          });
+          return;
+        })
+        .catch(() => {
+          this.$toast.e(`Houve algo de errado ao processar o arquivo ${file.name}`);
+        });
     },
   },
 };
 </script>
-
