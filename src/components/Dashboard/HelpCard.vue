9fd0ddd23ff1c5822cf9d31ecacea459884e1d29
Bruno Rodrigues <brunohrsilva1@gmail.com>
2020-12-03 21:37:30 -0300

    Fix tags

--- src/components/Dashboard/HelpCard.vue
+++ src/components/Dashboard/HelpCard.vue
@@ -1,35 +1,32 @@
 <template>
-  <v-card
-    max-height="300"
-    width="470"
-    class="mt-8"
-  >
+  <v-card max-height="300" width="470" class="mt-8">
     <div class="ma-8">
       <h5 class="h5-bold color-white">{{ title }}</h5>
       <sub-1 color="white" class="my-4">{{ description }}</sub-1>
-      <v-dialog v-model="dialog" width="500" scrollable>
+
+      <v-dialog v-model="dialog" width="600" scrollable>
         <template v-slot:activator="{ on }">
-          <v-btn color="transparent" dark v-on="on" style="text-decoration: underline">
-            {{ howDoesWork }}
-          </v-btn>
+          <g-btn
+            color="transparent"
+            :label="howDoesWork"
+            dark
+            v-on="on"
+            style="text-decoration: underline"
+          />
         </template>
 
         <v-card>
-          <v-card-title class="headline color-secondary" primary-title>
-            {{ howDoesWork }}
-          </v-card-title>
-
-          <v-card-text>
-            {{ modalText }}
-          </v-card-text>
+          <g-card-header
+            :title="howDoesWork"
+            :description="modalText"
+            class="headline color-secondary"
+          />
 
           <v-divider></v-divider>
 
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" flat @click="dialog = false">
-              {{ btnText }}
-            </v-btn>
+            <g-btn :label="btnText" type="primary" @click="dialog = false" />
           </v-card-actions>
         </v-card>
       </v-dialog>
