321dca4256a5cbf91cb26bedfafa9ee10e805161
João Henrique <joaoh9.costa@gmail.com>
2021-01-12 00:07:45 -0300

    questions padding

--- src/components/Static/Questions.vue
+++ src/components/Static/Questions.vue
@@ -2,13 +2,14 @@
   <div class="d-flex justify-center flex-wrap">
     <g-card-outlined borderColor="#1a193c" style="width: 400px" class="mx-2 my-2">
       <div class="d-flex align-center flex-column">
-        <h5 class="h5-bold text-center">{{ $t('questions.reachUs.title') }}</h5>
+        <h5 class="h5-bold text-center mt-n4">{{ $t('questions.reachUs.title') }}</h5>
         <sub-1 class="text-center my-4">{{ $t('questions.reachUs.description') }}</sub-1>
         <v-btn
           absolute
           bottom="0"
           color="primary"
           outlined
+          large
           class="font-weight-bold"
           @click="goToNewWindow($t('questions.reachUs.click'))"
         >
@@ -18,13 +19,14 @@
     </g-card-outlined>
     <g-card-outlined borderColor="#FF9200" style="width: 400px" class="mx-2 my-2">
       <div class="d-flex align-center flex-column">
-        <h5 class="h5-bold text-center">{{ $t('questions.call.title') }}</h5>
+        <h5 class="h5-bold text-center mt-n4">{{ $t('questions.call.title') }}</h5>
         <sub-1 class="text-center mt-4 mb-8">{{ $t('questions.call.description') }}</sub-1>
         <v-btn
           absolute
           bottom="0"
           color="secondary"
           outlined
+          large
           class="font-weight-bold"
           @click="goToNewWindow($t('questions.call.click'))"
         >
