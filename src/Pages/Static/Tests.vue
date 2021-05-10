4ef922f26d7c6e142f44303809e0ed2be140cfc0
João Henrique Costa <28352619+joaoh9@users.noreply.github.com>
2021-05-09 21:43:10 -0300

    update

--- src/Pages/Static/Tests.vue
+++ src/Pages/Static/Tests.vue
@@ -5,16 +5,20 @@
       <form-input title="Escreva a pergunta:"> </form-input>
       <v-text-field autofocus v-model="test.question" outlined />
       <form-input title="Descreva melhor a pergunta:"> </form-input>
-      <!-- <vue-editor :editorToolbar="$t('quill.defaultToolbar')" v-model="test.text" /> -->
-      <v-textarea outlined v-model="test.text" />
-      <div class="d-flex justify-space-between">
+      <vue-editor :editorToolbar="$t('quill.defaultToolbar')" v-model="test.text" />
+      <!-- <v-textarea outlined v-model="test.text" /> -->
+      <div class="d-flex justify-space-between mt-16">
         <form-input class="" title="Preencha as opções:"> </form-input>
         <form-input class="" title="Pontos pela pergunta"> </form-input>
       </div>
       <div v-for="(question, i) in test.options" :key="i">
         <v-row no-gutters>
           <v-col class=" flex-shrink-1 flex-grow-0" style="min-width: 70px">
-            <div class="mr-2 pa-5 secondary rounded-circle text-center">{{ i + 1 }}</div>
+            <div
+              class="mr-2 pa-5 rounded-circle text-center color-primary bg-color-secondary-lighten-1"
+            >
+              {{ i + 1 }}
+            </div>
           </v-col>
           <v-col class=" flex-grow-1">
             <v-text-field class="flex-grow-1" v-model="test.options[i].option" outlined />
@@ -30,8 +34,7 @@
           </v-col>
         </v-row>
       </div>
-      <g-btn outlined type="outlined" block @click="addQuestion" label="Adicionar pergunta">
-      </g-btn>
+      <g-btn outlined type="primary" block @click="addQuestion" label="Adicionar pergunta"> </g-btn>
       <div class="mt-4">
         <form-input class="" title="Dificuldade"> </form-input>
         <v-slider v-model="test.difficulty" thumb-label="always" min="0" max="10"></v-slider>
@@ -44,12 +47,12 @@
 </template>
 
 <script>
-// import { VueEditor } from 'vue2-editor';
+import { VueEditor } from 'vue2-editor';
 
 export default {
   name: 'tests',
   components: {
-    // VueEditor,
+    VueEditor,
   },
   data() {
     return {
